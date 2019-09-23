4fffbdb1a691ae042f2cc134f7bfa26208382b10
Manfredi Petruso <manfredi.petruso@fabvla.com>
2019-09-23 10:13:51 +0200

    Invoice language

--- ps_emailalerts.php
+++ ps_emailalerts.php
@@ -308,7 +308,7 @@ class Ps_EmailAlerts extends Module
             Hook::exec('actionPDFInvoiceRender', array('order_invoice_list' => $order_invoice_list));
             $pdf = new PDF($order_invoice_list, PDF::TEMPLATE_INVOICE, $this->context->smarty);
             $file_attachement['content'] = $pdf->render(false);
-            $file_attachement['name'] = Configuration::get('PS_INVOICE_PREFIX', (int)$order->id_lang, null, $order->id_shop).sprintf('%06d', $order->invoice_number).'.pdf';
+            $file_attachement['name'] = Configuration::get('PS_INVOICE_PREFIX', $id_lang, null, $order->id_shop).sprintf('%06d', $order->invoice_number).'.pdf';
             $file_attachement['mime'] = 'application/pdf';
             
         } else {
