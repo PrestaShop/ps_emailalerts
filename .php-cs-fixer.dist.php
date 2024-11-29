2d212c9e2762a479574dd2da6f7fa4b6a544cf3b
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-11-29 14:06:04 +0100

    Fix CS fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,17 @@
 <?php
 
-$config = new PrestaShop\CodingStandards\CsFixer\Config();
+$psConfig = new PrestaShop\CodingStandards\CsFixer\Config();
 
+$config = new PhpCsFixer\Config();
 $config
     ->setUsingCache(true)
+    ->setRiskyAllowed(true)
+    ->setRules(array_merge($psConfig->getRules(), [
+        'blank_lines_before_namespace' => false,
+        'nullable_type_declaration_for_default_null_value' => true,
+        'blank_line_after_opening_tag' => false,
+        'linebreak_after_opening_tag' => false,
+    ]))
     ->getFinder()
     ->in(__DIR__)
     ->exclude('vendor');
